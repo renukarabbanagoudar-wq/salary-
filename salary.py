a9211e69a5eb8eb944f18e9471854b173099ff04
Renuka <renuka.rabbanagoudar@gmail.com>
2025-11-27 15:10:24 +0530

    Create salary.py

---
+++ salary.py
@@ -0,0 +1,14 @@
+import sys
+if len(sys.argv)==2:
+  print("pyhton salary_bonus:",salary)
+  sys,exist(1)
+
+else:
+  salary=float(sys.argv[1])
+  expect ValueError:
+   print("Please enter a valid salary")
+  sys.exist(1)
+  bonus=salry*0.10
+  total_salary=salary+bouns
+  print("Bonus amount:",bonus)
+  print("Total Salary:",salary)
